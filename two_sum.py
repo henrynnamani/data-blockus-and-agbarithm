6995292fab46f4820ae0b288de5513d4d127ce28
Henry <hoyx0101@gmail.com>
2026-02-19 11:17:18 +0100

    Implement twoSum function to find indices of two numbers

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for index, number in enumerate(nums):
+            difference = target - number
+
+            if difference in seen:
+                return [seen[difference], index]
+
+            seen[number] = index
+
+        return []
